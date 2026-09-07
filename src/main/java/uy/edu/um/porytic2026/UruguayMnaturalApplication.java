33831d1d738750fbed6c28fdf6c393d4a62c0b5d
Jue Yin Wu <jwu@correo.um.edu.uy>
2026-09-07 10:09:40 -0300

    Name Refractor
    Cambia el nombre de la aplicación a UruguayMnatural

--- src/main/java/uy/edu/um/porytic2026/UruguayMnaturalApplication.java
+++ src/main/java/uy/edu/um/porytic2026/UruguayMnaturalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Porytic2026Application {
+public class UruguayMnaturalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Porytic2026Application.class, args);
+		SpringApplication.run(UruguayMnaturalApplication.class, args);
 	}
 
 }
--- src/test/java/uy/edu/um/porytic2026/UruguayMnaturalApplicationTests.java
+++ src/test/java/uy/edu/um/porytic2026/UruguayMnaturalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Porytic2026ApplicationTests {
+class UruguayMnaturalApplicationTests {
 
 	@Test
 	void contextLoads() {
